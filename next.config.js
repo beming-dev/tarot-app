6ce8d42ab120839264c7955d681b81021186729b
beming-dev <mingfordev@gmail.com>
2024-05-21 11:32:40 +0900

    Add

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const debug = process.env.NODE_ENV !== "production";
 const repository = "tarot-app";
 
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/tarot-app" : "",
+  basePath: process.env.NODE_ENV === "development" ? "" : "/tarot-app",
   reactStrictMode: true,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
