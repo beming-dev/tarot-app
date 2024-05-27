4abee539084f22035dc0633eb017d68f45be1e32
beming-dev <mingfordev@gmail.com>
2024-05-27 12:41:03 +0900

    Update google analytivs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,55 @@
 import Layout from "@/components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
+import * as gtag from "@/lib/gtag";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider>
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
-    </ChakraProvider>
+    <>
+      <Head>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <ChakraProvider>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </ChakraProvider>
+    </>
   );
 }
 
