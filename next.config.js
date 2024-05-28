802b4a6c1952875c0a859ed54bdd4fbcb13e9a81
beming-dev <mingfordev@gmail.com>
2024-05-28 23:15:23 +0900

    remove setting

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const repository = "tarot-app";
 
 const nextConfig = {
   // basePath: process.env.NODE_ENV === "development" ? "" : "/tarot-app",
-  reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  // reactStrictMode: true,
+  // assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  // trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
 module.exports = nextConfig;
